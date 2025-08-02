eaf958fd574811ba0209ac23c86cfd154ba3c2fb
Bocchi <139888492+Maysamaysa@users.noreply.github.com>
2025-08-02 13:15:08 +0700

    Update arena.java

--- arena.java
+++ arena.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class arena {
     public static void main(String[] args) {
-        Subscriber subscriber = new Subscriber();
-        Subscriber subscriber2 = new Subscriber();
+        Subscriber subscriber = new Subscriber(1);
+        Subscriber subscriber2 = new Subscriber(2);
 
         ScoreSource scoreSource = new ScoreSource();
         scoreSource.addScoreListener(subscriber);
         scoreSource.addScoreListener(subscriber2);
 
         Scanner scanner = new Scanner(System.in);
-        while(scanner != ""){
+        while(score != ""){
             scoreSource.setScoreLine(scanner.nextLine());
+            score = scanner.nextLine();
         }
     }
 }
